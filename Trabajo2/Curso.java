d8537523b94900623fbd012249675baaee93834b
Nacho <nachoagui527@gmail.com>
2025-08-29 17:02:29 -0300

    Segundo Trabajo

---
+++ Trabajo2/Curso.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Curso {
+    private String nombre;
+    private ArrayList<Estudiante> estudiantes;
+
+    public Curso(String nombre) {
+        this.nombre = nombre;
+        this.estudiantes = new ArrayList<>();
+    }
+
+    public Curso(String nombre, ArrayList<Estudiante> estudiantes) {
+        this.nombre = nombre;
+        this.estudiantes = estudiantes;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Estudiante> getEstudiantes() {
+        return estudiantes;
+    }
+
+    public void setEstudiantes(ArrayList<Estudiante> estudiantes) {
+        this.estudiantes = estudiantes;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso: " + nombre + ", Estudiantes: " + estudiantes;
+    }
+}
+
